c897041df90e8efa9e05689c665350604d793d3e
Ahmad1827 <ahmadarnaoute1896@gmail.com>
2026-08-22 13:01:55 +0300

    implemented full SBPL parser with multi argument support

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,23 @@
 module Main (main) where
 
+import Parser
+import qualified Data.Text.IO as TIO
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
+import Text.Megaparsec (errorBundlePretty)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  args <- getArgs
+  case args of
+    [filePath] -> do
+      content <- TIO.readFile filePath
+      case parseSandboxProfile filePath content of
+        Left err -> do
+          putStrLn (errorBundlePretty err)
+          exitFailure
+        Right profile -> do
+          print profile
+    _ -> do
+      putStrLn "Usage: sandscout <file.sb>"
+      exitFailure
