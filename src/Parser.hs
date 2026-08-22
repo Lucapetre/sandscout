ceb3eb3ffd26203fc181655b612231267a4dd30f
Ahmad1827 <ahmadarnaoute1896@gmail.com>
2026-08-22 13:01:55 +0300

    implemented full SBPL parser with multi argument support

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,6 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Parser where
+module Parser( parseSandboxProfile
+  , profileP
+  , ruleP
+  , filterP
+  ) where
 
 import Types
 
@@ -59,3 +63,85 @@ boolP = lexeme $
 decisionP :: Parser Decision
 decisionP = (symbol "allow" >> return Allow)
         <|> (symbol "deny" >> return Deny)
+
+filterValueP :: Parser FilterValue
+filterValueP = (RegexVal <$> regexPatternP)
+           <|> (StringVal <$> quotedStringP)
+           <|> (BoolVal <$> try boolP)
+           <|> (NumberVal <$> try numberP)
+           <|> (FilterVal <$> try filterP)
+           <|> parens (ListVal <$> many filterValueP)
+           <|> (StringVal <$> identifierP)
+
+subpathP :: Parser Filter
+subpathP = do
+  _ <- symbol "subpath"
+  paths <- some quotedStringP
+  let norm p = if T.isSuffixOf "/" p then p else p <> "/"
+  case map (Subpath . norm) paths of
+    [s] -> return s
+    multiple -> return $ RequireAny multiple
+
+literalP :: Parser Filter
+literalP = do
+  _ <- symbol "literal"
+  paths <- some quotedStringP
+  case map Literal paths of
+    [s] -> return s
+    multiple -> return $ RequireAny multiple
+
+regexP :: Parser Filter
+regexP = do
+  _ <- symbol "regex" <|> symbol "regex*"
+  pats <- some (regexPatternP <|> quotedStringP)
+  case map Regex pats of
+    [s] -> return s
+    multiple -> return $ RequireAny multiple
+
+filterP :: Parser Filter
+filterP = parens $
+      subpathP
+  <|> literalP
+  <|> regexP
+  <|> (symbol "require-all" >> RequireAll <$> many filterP)
+  <|> (symbol "require-any" >> RequireAny <$> many filterP)
+  <|> (symbol "require-not" >> RequireNot <$> filterP)
+  <|> (symbol "debug-mode" >> return DebugMode)
+  <|> (BooleanConst <$> boolP)
+  <|> do
+        name <- identifierP
+        args <- many filterValueP
+        return $ GenericFilter name args
+
+ruleP :: Parser Rule
+ruleP = parens $ do
+  dec <- decisionP
+  act <- identifierP
+  filts <- many filterP
+  let sanitizedAct = T.replace "*" "STAR" act
+  return $ Rule dec sanitizedAct filts
+
+versionP :: Parser Text
+versionP = parens $ do
+  _ <- symbol "version"
+  (T.pack . show <$> numberP) <|> identifierP
+
+defaultDecisionP :: Parser Decision
+defaultDecisionP = parens $ do
+  dec <- decisionP
+  _ <- symbol "default"
+  return dec
+
+profileP :: Parser SandboxProfile
+profileP = do
+  sc
+  ver <- optional versionP
+  defDec <- optional defaultDecisionP
+  rls <- many ruleP
+  eof
+  let finalVer = maybe "1" id ver
+  let finalDef = maybe Deny id defDec
+  return $ SandboxProfile finalVer finalDef rls
+
+parseSandboxProfile :: String -> Text -> Either (ParseErrorBundle Text Void) SandboxProfile
+parseSandboxProfile = runParser profileP
